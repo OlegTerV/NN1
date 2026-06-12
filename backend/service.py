bf7fadf4d6ae7953e1d82e1524fb9db3b2630fb4
OlegTerV <www.oleg2002ter@mail.ru>
2026-06-12 13:38:53 +0300

    Add fix

--- backend/service.py
+++ backend/service.py
@@ -4,6 +4,7 @@ import json
 import os
 import typing as t
 from pathlib import Path
+<<<<<<< Updated upstream
 
 import ultralytics
 import bentoml
@@ -30,3 +31,173 @@ class YoloService:
         output = image.parent.joinpath(f"{image.stem}_result{image.suffix}")
         result.save(str(output))
         return output
+=======
+import cv2
+import ultralytics
+import bentoml
+from bentoml.validators import ContentType
+import numpy as np
+import enum
+from embedding import SentenceTransformers, BentoMLEmbeddings
+import base64
+import uuid
+from qdrant_client import QdrantClient
+from qdrant_client.http import models
+from qdrant_client.models import Distance, VectorParams, PointStruct
+from PIL import Image
+import sys
+import io
+
+CustomImage = t.Annotated[Path, ContentType("image/*")]
+#os.environ["YOLO_MODEL"] = "yolov11n-face.pt"
+db_host = os.getenv("DB_SERVICE", "http://database:6333")
+
+class UserRequestStatus(enum.Enum):
+    something_went_wrong = 5
+    ok = 4
+    already_in_the_database = 3
+    no_face_in_the_photo = 2
+    is_not_registered = 1
+
+
+@bentoml.service( traffic={"timeout": 600}, resources={"gpu": 1})
+class YoloService:
+    client = QdrantClient(url=db_host)
+    #client = QdrantClient(":memory:")
+    db_collection_name = "LabWork42"
+
+    def __init__(self):
+        from ultralytics import YOLO
+        yolo_model = os.getenv("YOLO_MODEL", "yolov11n-face.pt")
+        self.model = YOLO(yolo_model)
+        embedding_service = SentenceTransformers()
+        self.embed_model = BentoMLEmbeddings(embedding_service)
+        self.client.create_collection(
+            collection_name = self.db_collection_name,
+            vectors_config=VectorParams(size=384, distance=Distance.COSINE)
+        )
+    
+    def render(self, image: CustomImage) -> CustomImage:
+        result = self.model.predict(image)[0]
+        output = image.parent.joinpath(f"{image.stem}_result{image.suffix}")
+        result.save(str(output))
+        return output
+    
+    @bentoml.api
+    def add_user(self, image: CustomImage) -> UserRequestStatus:
+        result = self.model.predict(image)[0]
+        faces = self.face_det(result.boxes.xyxy.tolist(), image)
+        if len(faces) == 0:
+            return UserRequestStatus.no_face_in_the_photo
+        
+        #crop_face_hoto = self.string2Image(self.img2string(faces[0]))
+
+        face_emb = self.face_vec_2(faces[0]) #emb для первого лица из списка "вырезанных" лиц
+
+        hits = self.client.query_points( #пытаемся получить самое похожее лицо
+            collection_name=self.db_collection_name,
+            query= face_emb,
+            limit=1
+        )
+        
+        if (len(hits.points)) == 0: #в бд нет лиц вообще
+            self.addUserToDB(face_emb, self.img2string(faces[0]))
+            return UserRequestStatus.ok 
+
+        if (hits.points[0].score>=0.88): #лицо с фото сильно похоже на найденное в бд лицо
+            return UserRequestStatus.already_in_the_database
+        else: 
+            self.addUserToDB(face_emb, self.img2string(faces[0]))
+            return UserRequestStatus.ok
+    
+    @bentoml.api
+    def find_user(self, image: CustomImage) -> UserRequestStatus:
+        result = self.model.predict(image)[0]
+        faces = self.face_det(result.boxes.xyxy.tolist(), image)
+        if len(faces) == 0:
+            return UserRequestStatus.no_face_in_the_photo
+        
+        face_emb = self.face_vec_2(faces[0])
+
+        hits = self.client.query_points( #пытаемся получить самое похожее лицо
+            collection_name=self.db_collection_name,
+            query= face_emb,
+            limit=1
+        )
+
+        if (len(hits.points)) == 0: #в бд нет лиц вообще
+            return UserRequestStatus.is_not_registered
+        
+        if (hits.points[0].score>=0.88): #лицо с фото сильно похоже на найденное в бд лицо
+            return UserRequestStatus.ok
+        else:
+            return UserRequestStatus.is_not_registered
+
+    def predict(self, images: list[CustomImage]) -> list[list[dict]]: #нужно ли это???
+        results = self.model.predict(source=images)
+        return [json.loads(result.tojson()) for result in results]
+    
+    def face_det(self, boxes: list[list[float]], srcImage: CustomImage) -> list[np.ndarray]:
+        img = cv2.imread(str(srcImage))
+        crop_faces_list = []
+
+        # crop method. Перебрать ограничивающие рамки
+        for i, box in enumerate(boxes):
+            print(i, box)
+            x1, y1, x2, y2 = box
+            crop_object = img[int(y1):int(y2), int(x1):int(x2)]
+            crop_faces_list.append(crop_object)
+
+        return crop_faces_list
+
+    def face_vec(self, face: np.ndarray) -> np.ndarray: 
+        oneLineArr = np.concatenate(np.concatenate(face)) #из многомерного numpy в одномерный numpy
+
+        strings_list = []
+        for color in oneLineArr:
+            strings_list.append(str(color))
+
+        face_vector = self.embed_model._get_query_embedding(strings_list)
+
+        return face_vector
+
+    def face_vec_2(self, face: np.ndarray) -> np.ndarray: 
+        img = Image.fromarray(face, mode='RGB')
+        buffer = io.BytesIO()
+        img.save(buffer, format="PNG")
+
+        img_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
+        face_vector = self.embed_model._get_query_embedding([img_base64])
+
+        return face_vector
+
+    def addUserToDB(self, vec: np.ndarray, image2String: str):
+        self.client.upsert(
+            collection_name=self.db_collection_name,
+            points=[
+                PointStruct(
+                    id=uuid.uuid4(),
+                    vector=vec,
+                    payload={"bin_data": image2String}
+                )
+            ]
+        )
+
+    def img2string(self, crop_photo: np.ndarray) -> str:
+        img = Image.fromarray(crop_photo)
+        b64string = ""
+        buffer = io.BytesIO()
+        img.save(buffer, format="PNG")
+
+        img_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
+        return b64string
+    
+    def string2Image(self, string: str) -> CustomImage:
+        f = io.BytesIO(base64.b64decode(string.encode('utf-8')))
+        pilimage = Image.open(f)
+
+        output_path = "crop_img.png"
+        result = pilimage.save(output_path) 
+
+        return result
+>>>>>>> Stashed changes
--- frontend/UI.py
+++ frontend/UI.py
@@ -4,6 +4,7 @@ import typing as t
 from bentoml.validators import ContentType
 import tempfile
 import os
+<<<<<<< Updated upstream
 
 src_image, dst_image = streamlit.columns(2)
 result = None
@@ -13,12 +14,33 @@ Image = t.Annotated[Path, ContentType("image/*")]
 upload_file = streamlit.file_uploader("Choose a file", type=["jpg", "jpeg", "png", "bmp", "webp"])
 
 with src_image:
+=======
+import enum
+
+Image = t.Annotated[Path, ContentType("image/*")]
+class UserRequestStatus(enum.Enum):
+    something_went_wrong = 5
+    ok = 4
+    already_in_the_database = 3
+    no_face_in_the_photo = 2
+    is_not_registered = 1
+
+src_image_add, dst_image_add = streamlit.columns(2)
+
+bentoml_host = os.getenv("BENTOML_SERVICE", "http://backend:3000")
+
+
+upload_file = streamlit.file_uploader("Choose a file", type=["jpg", "jpeg", "png", "bmp", "webp"])
+
+with src_image_add:
+>>>>>>> Stashed changes
     if upload_file is not None:
         streamlit.image(upload_file, channels="BGR", width=900)
         suffix = Path(upload_file.name).suffix
         with tempfile.NamedTemporaryFile(delete=False, suffix=suffix) as tmp_file:
             tmp_file.write(upload_file.getvalue())
             tmp_path = Path(tmp_file.name)
+<<<<<<< Updated upstream
 
         with bentoml.SyncHTTPClient(bentoml_host) as client:
             result = client.render(tmp_path)
@@ -26,3 +48,34 @@ with src_image:
 with dst_image:
     if upload_file is not None:
         streamlit.image(result, channels="BGR", width=900)
+=======
+            
+with dst_image_add:
+    if upload_file is not None:
+        if streamlit.button("Add user", width="stretch"):
+            with bentoml.SyncHTTPClient(bentoml_host, timeout=600) as client:
+
+                result = client.add_user(tmp_path)
+
+                if (result == UserRequestStatus.ok.value) :
+                    streamlit.success("Пользователь добавлен в базу данных")
+                    #streamlit.image(image, channels="BGR", width=300)
+                elif (result == UserRequestStatus.already_in_the_database.value) :
+                    streamlit.error("Пользователь уже есть в базе данных")
+                elif (result == UserRequestStatus.no_face_in_the_photo.value) :
+                    streamlit.warning("На фотографии нет лиц")
+
+
+        if streamlit.button("Find user", width="stretch"):
+            with bentoml.SyncHTTPClient(bentoml_host, timeout=600) as client:
+
+                result = client.find_user(tmp_path)
+
+                if (result == UserRequestStatus.ok.value) :
+                    streamlit.success("Пользователь есть в базе данных")
+                    #streamlit.image(image, channels="BGR", width=300)
+                elif (result == UserRequestStatus.is_not_registered.value) :
+                    streamlit.error("Пользователь не зарегестрирован")
+                elif (result == UserRequestStatus.no_face_in_the_photo.value) :
+                    streamlit.warning("На фотографии нет лиц")
+>>>>>>> Stashed changes
